8e2f599dadd2039cf6b1776cdc501bec02317054
William Brown <william@blackhats.net.au>
2022-07-20 15:45:36 +1000

    Fix up attestation serialisation

--- src/ctap2/attestation.rs
+++ src/ctap2/attestation.rs
@@ -531,36 +531,39 @@ impl Serialize for AttestationObject {
         S: Serializer,
     {
         // serializer.serialize_bytes(&v)
-        let mut map_len = 2;
-        if self.att_statement != AttestationStatement::None {
-            map_len += 1;
-        }
-
+        let map_len = 3;
         let mut map = serializer.serialize_map(Some(map_len))?;
 
-        map.serialize_entry(
-            &2,
-            &self
+        let auth_data =
+            self
                 .auth_data
                 .to_vec()
-                .map_err(|_| SerError::custom("Failed to serialize auth_data"))?,
+                .map(|v| serde_cbor::Value::Bytes(v))
+                .map_err(|_| SerError::custom("Failed to serialize auth_data"))?;
+
+        map.serialize_entry(
+            &"authData",
+            &auth_data
         )?;
         match self.att_statement {
             AttestationStatement::None => {
-                map.serialize_entry(&1, &"none")?;
+                // Even with Att None, an empty map is returned in the cbor!
+                map.serialize_entry(&"fmt", &"none")?;
+                let v = serde_cbor::Value::Map(std::collections::BTreeMap::new());
+                map.serialize_entry(&"attStmt", &v)?;
             }
             AttestationStatement::Packed(ref v) => {
-                map.serialize_entry(&1, &"packed")?;
-                map.serialize_entry(&3, v)?;
+                map.serialize_entry(&"fmt", &"packed")?;
+                map.serialize_entry(&"attStmt", v)?;
             }
             AttestationStatement::FidoU2F(ref v) => {
-                map.serialize_entry(&1, &"fido-u2f")?;
-                map.serialize_entry(&3, v)?;
+                map.serialize_entry(&"fmt", &"fido-u2f")?;
+                map.serialize_entry(&"attStmt", v)?;
             }
             AttestationStatement::Unparsed(ref v) => {
                 // Unparsed is currently only used in the fido1.0/u2f-case
-                map.serialize_entry(&1, &"fido-u2f")?;
-                map.serialize_entry(&3, v)?;
+                map.serialize_entry(&"fmt", &"fido-u2f")?;
+                map.serialize_entry(&"attStmt", v)?;
             }
         }
 
